f98ebd836850dda8512232c79b8f84412f99f26a
Francisco Nunes <up201908253@up.pt>
2023-04-19 19:10:24 +0100

    source

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInita8b073a3a7944ae27ae723647a901e52
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInita8b073a3a7944ae27ae723647a901e52', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInita8b073a3a7944ae27ae723647a901e52', 'loadClassLoader'));
